79522bade31cb17b469b6908f58c73fecac6007d
Rohan Chikorde <rohan.chikorde@gmail.com>
2019-06-05 13:21:52 +0530

    Update Activity05.R
    added set.seed for consistent results and formatting

--- Lesson02/Activity05/Activity05.R
+++ Lesson02/Activity05/Activity05.R
@@ -1,13 +1,14 @@
 # install.packages("klaR")
+set.seed(1)
 library(klaR)
-ms<-read.csv('./Lesson02/Activity05/mushrooms.csv')
+
+ms <- read.csv('./Lesson02/Activity05/mushrooms.csv')
 dim(ms)
+
 summary.data.frame(ms)
-ms_k<-ms[,2:23]
+ms_k <- ms[, 2:23]
 
+kmodes_ms <- kmodes(ms_k, 2)
 
-kmodes_ms<-kmodes(ms_k,2)
-
-result<-table(ms$class, kmodes_ms$cluster)
+result <- table(ms$class, kmodes_ms$cluster)
 result
-
