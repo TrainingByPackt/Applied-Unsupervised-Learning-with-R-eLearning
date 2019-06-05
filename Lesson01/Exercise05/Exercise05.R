de90ef6c258294bcbaba14f68541f12c331781c3
Rohan Chikorde <rohan.chikorde@gmail.com>
2019-06-05 13:18:05 +0530

    Update Exercise05.R
    changed to appropriate variable name "km.res" as per pdf and added formatting to the code

--- Lesson01/Exercise05/Exercise05.R
+++ Lesson01/Exercise05/Exercise05.R
@@ -1,8 +1,9 @@
 library("cluster")
 library("factoextra")
 
-iris_data<-iris[,1:2]
-
-km<-pam(iris_data,3)
-
-fviz_cluster(km, data = iris_data,palette = "jco",ggtheme = theme_minimal())
+iris_data <- iris[, 1:2]
+km.res <- pam(iris_data, 3)
+fviz_cluster(km,
+             data = iris_data,
+             palette = "jco",
+             ggtheme = theme_minimal())
